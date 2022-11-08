c33b08534d27629ba9d1dad9dc10ead85356b729
Morgado Add <113382227+Morgado18@users.noreply.github.com>
2022-11-07 18:55:51 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const h2 = document.querySelector('#h2');
+
+const jump = ()=>{
+  mario.classList.add('jump');
+
+  setTimeout(()=>{
+    mario.classList.remove('jump');
+  },500)
+}
+
+const loop = setInterval(()=>{
+
+  const pipePostion = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
+
+  if(pipePostion <= 120 && pipePostion > 0 && marioPosition < 80){
+    pipe.style.animation='none'
+    pipe.style.left=` ${pipePostion}px`
+
+    mario.style.animation='none'
+    mario.style.bottom=` ${marioPosition}px`
+
+    mario.src= 'game-over.png'
+    mario.style.width = '80px'
+    mario.style.marginLeft = '50px'
+    h2.style.display="block"
+
+    clearInterval(loop);
+  } 
+},10)
+
+document.addEventListener('keydown', jump);
+
